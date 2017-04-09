a449de6232747e31ba144cdd22266a4cf4ea14fe
Ziyan Mo <ziyanmo@guest-wireless-207-151-057-086.usc.edu>
2017-04-08 18:58:09 -0700

    hi

---
+++ flask-boilerplate/_updated/app/controllers/pages.py
@@ -0,0 +1,25 @@
+from flask import render_template, Blueprint, request
+from app.forms import *
+
+blueprint = Blueprint('pages', __name__)
+
+
+################
+#### routes ####
+################
+
+
+@blueprint.route('/')
+def home():
+    return render_template('pages/placeholder.home.html')
+
+
+@blueprint.route('/about')
+def about():
+    return render_template('pages/placeholder.about.html')
+
+
+@blueprint.route('/login')
+def login():
+    form = LoginForm(request.form)
+    return render_template('forms/graph.html', form=form)
