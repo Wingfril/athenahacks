5a20f1c1682135818c44baa11520cbadd2e5366c
Tina Wang <tswang@caltech.edu>
2017-04-08 20:29:05 -0700

    Deleted Flask

--- flask-boilerplate/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import DataRequired, EqualTo, Length
-
-# Set your classes here.
-
-class GraphForm(Form):
-    temp = TextField('Temperature', [DataRequired()])
